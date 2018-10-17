f63fadbd7507837694ab08d9a801440c44df1c18
Kevin Smith <ksmithbaylor@gmail.com>
2018-10-17 00:29:32 -0400

    Draw the rest of the owl

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Barcode from 'react-barcode';
 import './App.css';
 
+const randomVinUrl = type =>
+  `https://cors-anywhere.herokuapp.com/randomvin.com/getvin.php?type=${type}`;
+
 class App extends Component {
+  state = {
+    vin: null,
+    loading: false
+  };
+
+  fetchVin(type) {
+    console.log('fetching', type);
+    this.setState({ loading: true }, () => {
+      fetch(randomVinUrl(type))
+        .then(response => response.text())
+        .then(vin => {
+          this.setState({ vin, loading: false });
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    });
+  }
+
+  handleReal = this.fetchVin.bind(this, 'real');
+  handleFake = this.fetchVin.bind(this, 'fake');
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <VinButtons onReal={this.handleReal} onFake={this.handleFake} />
+        <BarcodeDisplay vin={this.state.vin} loading={this.state.loading} />
+        <Info />
       </div>
     );
   }
+}
+
+function VinButtons({ onReal, onFake }) {
+  return (
+    <div className="VinButtons">
+      <button onClick={onFake}>Fake VIN</button>
+      <button onClick={onReal}>Real VIN</button>
+    </div>
+  );
+}
+
+function BarcodeDisplay({ vin, loading }) {
+  const contents = loading ? (
+    <div className="spinner" />
+  ) : !vin ? null : (
+    <Barcode value={vin} />
+  );
+  return <div className="BarcodeDisplay">{contents}</div>;
+}
+
+function Info() {
+  const randomVinLink = (
+    <a href="http://randomvin.com" target="_blank" rel="noopener noreferrer">
+      RandomVIN.com
+    </a>
+  );
+
+  const githubLink = (
+    <a
+      href="https://github.com/ksmithbaylor/vin-barcodes"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
+  );
+
+  return (
+    <footer>
+      Driven by {randomVinLink}. Made by Kevin Smith, who was tired of Googling
+      "vin barcode" during development. Code on {githubLink}.
+    </footer>
+  );
 }
 
 export default App;
