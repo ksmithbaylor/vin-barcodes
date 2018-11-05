be68703944396013de6f5f8a3128ea1793c2aeee
Kevin Smith <ksmithbaylor@gmail.com>
2018-11-05 14:25:17 -0500

    Move lambda helpers into the function

--- src/lambda-helpers.js
+++
@@ -1,9 +0,0 @@
-export const success = body => ({
-  statusCode: 200,
-  body: JSON.stringify(body)
-});
-
-export const failure = err => ({
-  statusCode: 500,
-  body: JSON.stringify(err.message)
-});
--- src/lambdas/randomVin.js
+++ src/lambdas/randomVin.js
@@ -1,5 +1,4 @@
 import 'isomorphic-fetch';
-import { success, failure } from '../lambda-helpers';
 
 const getVin = type =>
   fetch(`http://randomvin.com/getvin.php?type=${type}`).then(res => res.text());
@@ -7,8 +6,14 @@ const getVin = type =>
 export async function handler(event) {
   try {
     const vin = await getVin(event.queryStringParameters.type);
-    return success({ vin });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ vin })
+    };
   } catch (err) {
-    return failure(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err.message)
+    };
   }
 }
